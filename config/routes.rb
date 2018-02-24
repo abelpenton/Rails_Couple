bd259176566a1c7c0d9f7aa4630c06032bf4864a
abelpenton <2235penton@gmail.com>
2018-02-23 18:21:48 -0800

    special notation in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'article/index'
+  get "special", to: "article#index"
+
+  
 
   root 'article#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
