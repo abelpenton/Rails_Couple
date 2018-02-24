91e472557648be4f2783e9e7315932e49c212505
abelpenton <2235penton@gmail.com>
2018-02-23 18:47:36 -0800

    implement get alls View and details view with method details in Controller

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,5 +1,12 @@
 class ArticleController < ApplicationController
+  
+  #Get /articles
   def index
     @articles = Article.all
+  end
+
+  #Get /articles/:id
+  def show
+    @article = Article.find(params[:id])
   end
 end
