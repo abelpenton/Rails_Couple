52f91349614ce9a32ea73c2910b9217606d30df6
abelpenton <2235penton@gmail.com>
2018-03-02 14:58:09 -0800

    add gem devise and create models and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'articles/index'
   get "special", to: "articles#index"
 
+  
   resources :articles do
     resources :comments
   end
 
   root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
