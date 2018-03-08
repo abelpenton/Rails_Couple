30c4dca7e5670515496d6181d62cfe409b1720f4
abelpenton <2235penton@gmail.com>
2018-03-07 20:39:37 -0800

    add some migration in user,problem with avatar_uploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,6 @@ class UsersController < ApplicationController
   
     def edit
       @user = current_user
-      @ethnicities = Ethnicity.all
-      @horoscopes = Horoscope.all
     end
   
     def update
@@ -24,8 +22,6 @@ class UsersController < ApplicationController
   
     private
     def allowed_params #whitelist
-      params.require(:user).permit(:first_name, :last_name, :username, :age, :occupation, 
-      :type, :preference, :image_url, :about, :height, :imperial_height, :horoscope_id,  :smoking, :avatar, 
-      :ethnicity_ids => [], :language_ids => []) #for each attribute in the model /white_listed
+      params.require(:user).permit(:firstname, :username, :age , :image_url, :avatar) #for each attribute in the model /white_listed
     end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302184934) do
+ActiveRecord::Schema.define(version: 20180308042709) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20180302184934) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "firstname"
+    t.integer "age"
+    t.string "avatar"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
